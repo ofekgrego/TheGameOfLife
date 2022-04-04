e8101af51672322b67a04156751927d18d8f2281
ofekgrego <ofek.grego@gmail.com>
2022-04-04 23:39:44 +0300

    🏠 Added Function to Check Num of Neighbours

--- Functions.py
+++ Functions.py
@@ -17,3 +17,53 @@ def MousePressed(event):
     else:
         v.grid[pressed_block_x][pressed_block_y] = 0
         v.canvas.delete(v.grid_of_objects[pressed_block_x][pressed_block_y])
+
+    CheckNeighbours(pressed_block_x, pressed_block_y)
+
+def CheckNeighbours(place_x,place_y):
+    #Check if need to change
+    count_of_live_neighbours = 0
+    dir = [False,False,False,False] #L R U D
+    if(place_x != 0):
+        if(v.grid[place_x - 1][place_y] == 1):
+            count_of_live_neighbours += 1
+            # print("Can Left")
+        dir[0] = True
+    if (place_x != v.rows_columns_num-1):
+        if (v.grid[place_x + 1][place_y] == 1):
+            count_of_live_neighbours += 1
+            # print("Can Right")
+        dir[1] = True
+    if(place_y != 0):
+        if (v.grid[place_x][place_y - 1] == 1):
+            count_of_live_neighbours += 1
+            # print("Can Up")
+        dir[2] = True
+    if (place_y != v.rows_columns_num-1):
+        if (v.grid[place_x][place_y + 1] == 1):
+            count_of_live_neighbours += 1
+            # print("Can Down")
+        dir[3] = True
+
+    if(dir[0] & dir[2]):
+        if (v.grid[place_x - 1][place_y - 1] == 1):
+            count_of_live_neighbours += 1
+            # print("Can LeftUp")
+    if(dir[1] & dir[2]):
+        if (v.grid[place_x + 1][place_y - 1] == 1):
+            count_of_live_neighbours += 1
+            # print("Can RightUp")
+    if(dir[0] & dir[3]):
+        if (v.grid[place_x - 1][place_y + 1] == 1):
+            count_of_live_neighbours += 1
+            # print("Can LeftDown")
+    if(dir[1] & dir[3]):
+        if (v.grid[place_x + 1][place_y + 1] == 1):
+            count_of_live_neighbours += 1
+            # print("Can RightDown")
+    return count_of_live_neighbours
+
+
+def MoveGen():
+    # move all the tiles a gen
+    print("Move Gen")
